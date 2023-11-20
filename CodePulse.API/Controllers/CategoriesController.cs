5b1b347ed7e73af573c2b62598a52059565a3d65
Dominic James <30402312+dom3691@users.noreply.github.com>
2023-11-20 18:14:16 +0100

    Addedd Dtos for createcategories

---
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,45 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoriesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        //
+        [HttpPost]
+        public async Task<IActionResult> CreatCategory(CreateCategoryRequestDto request)
+        {
+            //Map DTo to Domain Model
+
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await dbContext.Categories.AddAsync(category);
+            await dbContext.SaveChangesAsync();
+
+            //Map From Domain Model to DTo
+
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+            return Ok(response);
+        }
+    }
+}
